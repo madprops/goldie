b716286d1bd71b1a6ef3514f02bb4cb85a8ad40f
madprops <easywest@protonmail.com>
2026-04-03 17:56:32 -0600

    Mods

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Goldie - Find text in files recursively";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.buildNimPackage {
+            pname = "goldie";
+            version = "unstable";
+            src = ./.;
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nim
+              pkgs.nimble
+            ];
+          };
+        });
+    };
+}
